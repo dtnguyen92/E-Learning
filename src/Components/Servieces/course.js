599a29fe81f790fb85e9ef26ca228820a0cc035f
nguyenstunt <nguyendt0902@gmail.com>
2020-03-10 19:12:27 +0700

    Update

--- src/Components/Servieces/course.js
+++ src/Components/Servieces/course.js
@@ -5,31 +5,40 @@ class CourseService {
         return Axios({
             method: 'GET',
             url: `https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayThongTinKhoaHoc?maKhoaHoc=${id}`,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
         })
     }
     fetchCourse() {
         return Axios({
             method: 'GET',
             url: `https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP11`,
-
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
         })
     }
     fetchCategory() {
         return Axios({
             method: 'GET',
             url: 'https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhMucKhoaHoc',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
         })
     }
     fetchCategoryDetail(name) {
         return Axios({
             method: 'GET',
             url: `http://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayKhoaHocTheoDanhMuc?maDanhMuc=${name}&MaNhom=GP01`,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
         })
     }
     fetchCourseSearch(keyword) {
         return Axios({
             method: "GET",
             url: `http://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?tenKhoaHoc=${keyword}&MaNhom=GP01`,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
         })
     }
 }
